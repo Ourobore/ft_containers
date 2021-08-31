fa805904966d2fbb078ffc8f4fc6909d79222f2d
Ourobore <chaprenet.loic@gmail.com>
2021-08-31 16:10:15 +0200

    Continued vector

--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: lchapren <lchapren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 13:24:09 by lchapren          #+#    #+#             */
-/*   Updated: 2021/08/31 13:43:18 by lchapren         ###   ########.fr       */
+/*   Updated: 2021/08/31 16:09:44 by lchapren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,22 @@
 
 namespace ft
 {
-	template < class T, class Allocator = std::allocator<T> > 
+	template < class T, class Allocator = std::allocator<T> >
 	class vector
 	{
-		
+		public:
+			typedef T 				value_type;
+			typedef Allocator		allocator_type;
+			typedef allocator_type::reference			reference;
+			typedef allocator_type::const_reference		const_reference;
+			typedef allocator_type::pointer				pointer;
+			typedef allocator_type::const_pointer		const_pointer;
+			typedef 									iterator;
+			typedef 									const_iterator;
+			typedef reverse_iterator<iterator>					reverse_iterator;
+			typedef reverse_iterator<const_iterator>			const_reverse_operator;
+			typedef iterator_trait<iterator>::difference_type	difference_type;
+			typedef difference_type	size_type
 	};
 }
 
