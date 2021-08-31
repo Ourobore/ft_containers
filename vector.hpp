7f223ca9b2ee44c7cc8dda970f8243730f922b42
Ourobore <chaprenet.loic@gmail.com>
2021-08-31 13:44:24 +0200

    Started ft_container. Created files for ft::vector

---
+++ vector.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchapren <lchapren@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/31 13:24:09 by lchapren          #+#    #+#             */
+/*   Updated: 2021/08/31 13:43:18 by lchapren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+# include <memory>
+
+namespace ft
+{
+	template < class T, class Allocator = std::allocator<T> > 
+	class vector
+	{
+		
+	};
+}
+
+#endif
