c8f5aec67e5f478888bbf4055e720e3ab50575ad
Ourobore <chaprenet.loic@gmail.com>
2021-09-03 12:05:03 +0200

    Found the syntax to separate function declaration and definition. Operator[] done

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: lchapren <lchapren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 13:23:12 by lchapren          #+#    #+#             */
-/*   Updated: 2021/09/02 17:18:26 by lchapren         ###   ########.fr       */
+/*   Updated: 2021/09/03 11:44:43 by lchapren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,7 @@
 int main()
 {
 	ft::vector<int> test(3, 4);
+	//std::cout << test[0] << std::endl;
+	//std::cout << test[1] << std::endl;
+	//std::cout << test[2] << std::endl;
 }
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: lchapren <lchapren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 13:24:09 by lchapren          #+#    #+#             */
-/*   Updated: 2021/09/02 17:23:54 by lchapren         ###   ########.fr       */
+/*   Updated: 2021/09/03 12:00:20 by lchapren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,46 +18,87 @@
 
 namespace ft
 {
-	template < class T, class Allocator = std::allocator<T> >
-	class vector
-	{
-		public:
-			typedef T 					value_type;
-			typedef Allocator			allocator_type;
-			typedef value_type&			reference;
-			typedef const value_type&	const_reference;
-			typedef value_type*			pointer;
-			typedef const value_type*	const_pointer;
-			typedef std::ptrdiff_t			difference_type;
-			typedef std::size_t				size_type;
 
-			//typedef 					iterator;
-			//typedef 					const_iterator;
-			//typedef reverse_iterator<iterator>			reverse_iterator;
-			//typedef reverse_iterator<const_iterator>	const_reverse_operator;
+template < class T, class Allocator = std::allocator<T> >
+class vector
+{
+	public:
+		typedef T 					value_type;
+		typedef Allocator			allocator_type;
+		typedef value_type&			reference;
+		typedef const value_type&	const_reference;
+		typedef value_type*			pointer;
+		typedef const value_type*	const_pointer;
+		typedef std::ptrdiff_t		difference_type;
+		typedef std::size_t			size_type;
 
-		protected:
-			pointer			_c;
-			size_type		_size;
-			size_type		_capacity;
-			allocator_type	_alloc;
+		//typedef 					iterator;
+		//typedef 					const_iterator;
+		//typedef reverse_iterator<iterator>			reverse_iterator;
+		//typedef reverse_iterator<const_iterator>	const_reverse_operator;
 
-		public:
-			explicit	vector(const allocator_type& alloc = allocator_type()) : _size(0), _capacity(0), _alloc(alloc)
-			{
-				_c = _alloc.allocate(0);
-			}
-			explicit	vector(size_type n, const_reference val = value_type(),
-								const allocator_type& alloc = allocator_type()) : _size(n), _capacity(n), _alloc(alloc)
-			{
-				_c = _alloc.allocate(_size);
-				for (size_type i = 0; i < _size; i++)
-				{
-					_alloc.construct(&_c[i], val);
-					std::cout << _c[i] << std::endl;
-				}			
-			}
-	};
+	protected:
+		pointer			_c;
+		size_type		_size;
+		size_type		_capacity;
+		allocator_type	_alloc;
+
+	public:
+		// Constructors
+		explicit	vector(const allocator_type& alloc = allocator_type());
+		explicit	vector(size_type n, const_reference val = value_type(), \
+							const allocator_type& alloc = allocator_type());
+
+		// Capacity
+		size_type	size() const;
+
+		// Overloads
+		reference		operator[](size_type n);
+		const_reference	operator[](size_type n) const;
+};
+
+// Constructors
+template < class T, class Allocator >
+vector<T, Allocator>::vector(const allocator_type& alloc) : _size(0), _capacity(0), _alloc(alloc)
+{
+	_c = _alloc.allocate(0);
+}
+
+template < class T, class Allocator >
+vector<T, Allocator>::vector(size_type n, const_reference val, const allocator_type& alloc) \
+: _size(n), _capacity(n), _alloc(alloc)
+{
+	_c = _alloc.allocate(_size);
+	for (size_type i = 0; i < _size; i++)
+		_alloc.construct(&_c[i], val);
+}
+
+// Capacity
+template < class T, class Allocator >
+typename vector<T, Allocator>::size_type	size() const
+{
+	return (_size);
+}
+
+// Overloads
+template < class T, class Allocator >
+typename vector<T, Allocator>::reference	vector<T, Allocator>::operator[](size_type n)
+{
+	if (n < 0 || n >= _size)
+		throw std::out_of_range("Index is out of range");
+	else
+		return (_c[n]);
+}
+
+template < class T, class Allocator >
+typename vector<T, Allocator>::const_reference	vector<T, Allocator>::operator[](size_type n) const
+{
+	if (n < 0 || n >= _size)
+		throw std::out_of_range("Index is out of range");
+	else
+		return (_c[n]);
+}
+
 }
 
 #endif
