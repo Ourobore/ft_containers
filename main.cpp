45b8ea65127d343a7f4a82379d4cb3d9bb284ba2
Ourobore <chaprenet.loic@gmail.com>
2021-09-11 11:31:51 +0200

    Added constructor in return of begin and end. Made some tests in main with iterators and const_iterators

--- main.cpp
+++ main.cpp
@@ -6,184 +6,83 @@
 /*   By: lchapren <lchapren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 13:23:12 by lchapren          #+#    #+#             */
-/*   Updated: 2021/09/10 10:31:48 by lchapren         ###   ########.fr       */
+/*   Updated: 2021/09/11 11:24:53 by lchapren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.hpp"
 #include <vector>
 
+template <class Container>
+void	printContainer(Container c)
+{
+	std::cout << "=============================================" << std::endl;
+	std::cout << "Size: [" << c.size() << "]   Capacity: [" << c.capacity() << "]" << std::endl;
+	std::cout << "[ ";
+	for (typename Container::size_type i = 0; i < c.size(); ++i)
+	{
+		std::cout << c[i];
+		if (i < c.size() - 1)
+			std::cout << ", ";
+	}
+	std::cout << " ]" << std::endl;
+	std::cout << "=============================================" << std::endl;
+}
+
 int main()
 {
-	/*
-	ft::vector<int> test;
-	test.push_back(4);
-	test.push_back(5);
-	test.push_back(6);
-	test.push_back(42);
-	test.push_back(-21);
-	test.push_back(69);
-
-	std::cout << std::endl;
-	std::cout << "test" << std::endl;
-	for (ft::vector<int>::iterator it = test.begin(); it != test.end(); ++it)
-		std::cout << *it << std::endl;
-		*/
-
 {
-	ft::vector<int> vct(10);
-	ft::vector<int> vct2;
-
-	for (unsigned long int i = 0; i < vct.size(); ++i)
-		vct[i] = (vct.size() - i) * 3;
-	std::cout << "=============================== 1" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	//printSize(vct);
-
-	vct2.insert(vct2.end(), 42);
-	std::cout << "=============================== 2" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	vct2.insert(vct2.begin(), 2, 21);
-	std::cout << "=============================== 3" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	//printSize(vct2);
-
-	vct2.insert(vct2.end() - 2, 42);
-	std::cout << "=============================== 4" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	//printSize(vct2);
-
-	vct2.insert(vct2.end(), 2, 84);
-	std::cout << "=============================== 5" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	//printSize(vct2);
-
-	vct2.resize(4);
-	std::cout << "=============================== 6" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "=============================== " << std::endl;
-	//printSize(vct2);
-
-	vct2.insert(vct2.begin() + 2, vct.begin(), vct.end());
-	std::cout << "=============================== 7" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	vct.clear();
-	std::cout << "=============================== 8" << std::endl;
-	std::cout << "size: " << vct.size() << std::endl;
-	std::cout << "capacity: " << vct.capacity() << std::endl;
-	for (size_t i = 0; i < vct.size(); ++i)
-		std::cout << vct[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	//printSize(vct2);
-
-	//printSize(vct);
-	//return (0);
+// Non const iterator with non const vector work
+ft::vector<int> v;
+ft::vector<int>::iterator it = v.begin();
+ft::vector<int>::iterator ite = v.end();
 }
+{
+// Const iterator with const vector work
+const ft::vector<int> v;
+ft::vector<int>::const_iterator cit = v.begin();
+ft::vector<int>::const_iterator cite = v.end();
+}
+{
+// Const iterator with non const vector doesn't work
+// Conversion needed?
+ft::vector<int> v;
+ft::vector<int>::const_iterator cit = v.begin();
+ft::vector<int>::const_iterator cite = v.end();
+}
+{
+// Non const iterator with const vector doesn't work
+// Conversion needed?
+const ft::vector<int> v;
+ft::vector<int>::iterator it = v.begin();
+ft::vector<int>::iterator ite = v.end();
+}
+/*
+{
+	ft::vector<int>	v;
+	for (int i = 0; i < 10; ++i)
+		v.push_back(i);
+
+	ft::vector<int>::iterator it = v.begin();
+	ft::vector<int>::iterator ite = v.end();
+
+	//for (; it != ite; ++it)
+	//	std::cout << *it << std::endl;
+
+	const ft::vector<int> vv(3, 4);
+	ft::vector<int>::iterator cit = vv.begin();
+	ft::vector<int>::iterator cite = vv.end();
 	
+	for (; cit != cite; ++cit)
+		std::cout << *cit << std::endl;
+
+	printContainer(v);
+}
+*/	
 	std::cout << std::endl << "################################" << std::endl;;
 	std::cout << "################################" << std::endl;;
 	std::cout << "################################" << std::endl << std::endl;;
 {
 }
-	std::vector<int> vct(10);
-	std::vector<int> vct2;
 
-	for (unsigned long int i = 0; i < vct.size(); ++i)
-		vct[i] = (vct.size() - i) * 3;
-	std::cout << "=============================== 1" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	//printSize(vct);
-
-	vct2.insert(vct2.end(), 42);
-	std::cout << "=============================== 2" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	vct2.insert(vct2.begin(), 2, 21);
-	std::cout << "=============================== 3" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	//printSize(vct2);
-
-	vct2.insert(vct2.end() - 2, 42);
-	std::cout << "=============================== 4" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	//printSize(vct2);
-
-	vct2.insert(vct2.end(), 2, 84);
-	std::cout << "=============================== 5" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	//printSize(vct2);
-
-	vct2.resize(4);
-	std::cout << "=============================== 6" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	//printSize(vct2);
-
-	vct2.insert(vct2.begin() + 2, vct.begin(), vct.end());
-	std::cout << "=============================== 7" << std::endl;
-	std::cout << "size: " << vct2.size() << std::endl;
-	std::cout << "capacity: " << vct2.capacity() << std::endl;
-	for (size_t i = 0; i < vct2.size(); ++i)
-		std::cout << vct2[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	vct.clear();
-	std::cout << "===============================8" << std::endl;
-	std::cout << "size: " << vct.size() << std::endl;
-	std::cout << "capacity: " << vct.capacity() << std::endl;
-	for (size_t i = 0; i < vct.size(); ++i)
-		std::cout << vct[i] << std::endl;
-	std::cout << "===============================" << std::endl;
-	//printSize(vct2);
-
-	//printSize(vct);
-	//return (0);
 }
--- vector.hpp
+++ vector.hpp
@@ -6,7 +6,7 @@
 /*   By: lchapren <lchapren@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 13:24:09 by lchapren          #+#    #+#             */
-/*   Updated: 2021/09/10 16:31:23 by lchapren         ###   ########.fr       */
+/*   Updated: 2021/09/11 10:59:34 by lchapren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,12 @@ template < class T, class Allocator = std::allocator<T> >
 class vector
 {
 	public:
-		typedef T 					value_type;
-		typedef Allocator			allocator_type;
-		typedef value_type&			reference;
-		typedef const value_type&	const_reference;
-		typedef value_type*			pointer;
-		typedef const value_type*	const_pointer;
+		typedef T 			value_type;
+		typedef Allocator	allocator_type;
+		typedef typename allocator_type::reference			reference;
+		typedef typename allocator_type::const_reference	const_reference;
+		typedef typename allocator_type::pointer			pointer;
+		typedef typename allocator_type::const_pointer	const_pointer;
 		typedef std::ptrdiff_t		difference_type;
 		typedef std::size_t			size_type;
 	
@@ -179,25 +179,27 @@ vector<T, Allocator>::~vector()
 template < class T, class Allocator >
 typename vector<T, Allocator>::iterator	vector<T, Allocator>::begin()
 {
-	return (_c);
+	return iterator(_c);
 }
 
 template < class T, class Allocator >
 typename vector<T, Allocator>::const_iterator	vector<T, Allocator>::begin() const
 {
-	return (_c);
+	std::cout << "cbegin" << std::endl;
+	return const_iterator(_c);
 }
 
 template < class T, class Allocator >
 typename vector<T, Allocator>::iterator	vector<T, Allocator>::end()
 {
-	return (_c + _size);
+	return iterator(_c + _size);
 }
 
 template < class T, class Allocator >
 typename vector<T, Allocator>::const_iterator	vector<T, Allocator>::end() const
 {
-	return (_c + _size);
+	std::cout << "cend" << std::endl;
+	return const_iterator(_c + _size);
 }
 
 
